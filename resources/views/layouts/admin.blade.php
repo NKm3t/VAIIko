4d83ebcfb59800dfbeeba1bb8b42ce13ff5b4a69
Norbert Kmet <norbertkmet@gmail.com>
2022-11-09 21:34:56 +0100

    Spraveny admin panel

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Dekorácie Lussy</title>
+
+
+    <!-- Scripts -->
+    <script src="Admin/js/popper.min.js"></script>
+    <script src="Admin/js/bootstrap.min.js"></script>
+    <script src="Admin/js/perfect-scrollbar.min.js"></script>
+    <script src="Admin/js/smooth-scrollbar.min.js"></script>
+    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="Admin/js/material-dashboard.min.js"></script>
+
+
+    <!--       Fonts and icons       -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <!-- Nucleo Icons -->
+    <link href="Admin/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="Admin/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+
+    <!-- Style -->
+    <link href="Admin/css/material-dashboard.css" rel="stylesheet" />
+
+</head>
+<body>
+
+    <div class="g-sidenav-show ">
+        @include('layouts.inc.sidebar')
+
+        <div class="main-content">
+            @include('layouts.inc.adminnav')
+
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+
+            @include('layouts.inc.adminfooter')
+        </div>
+    </div>
+
+    @yield('scripts')
+</body>
+</html>
---
+++ resources/views/layouts/inc/adminnav.blade.php
@@ -0,0 +1,54 @@
+<!-- Navbar -->
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
+    <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+
+            </ol>
+            <h6 class="font-weight-bolder mb-0">Dashboard</h6>
+        </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+            <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                <div class="input-group input-group-outline">
+                    <label class="form-label">Type here...</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <ul class="navbar-nav  justify-content-end">
+                <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                    <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                        <div class="sidenav-toggler-inner">
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Auth::user()->name }}
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">
+                                Môj účet
+                            </a></li>
+                        <li><a class="dropdown-item" href="#">
+                                Nastavenia
+                            </a></li>
+                        <li><a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Odhlásiť sa') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
+
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- End Navbar -->
