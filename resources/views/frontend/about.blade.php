055db6febbcb4b45f0aad4c6a0cf891b0570a437
Norbert Kmet <norbertkmet@gmail.com>
2023-01-16 14:43:21 +0100

    Doplnene info o platbe

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -17,10 +17,14 @@
                             Naše výrobky sú vhodné ako dekorácia do vášho  domu či bytu alebo aj ako darček na rôzne sviatky ako sú Vianoce, Veľká noc a rôzne životne udalosti (narodeniny, meniny, narodenie bábätka, životne jubileum, výročie, svadba, prvé sväté prijímanie, birmovku a iné). Taktiež sa venujeme výrobe zápichov na torty v rôznych rozmeroch podľa vášho želania. Drobné výrezy môžu slúžiť na dotvorenie vašich výrobkov či dekorácií ako napríklad aj na ikebany či vence na Sviatok všetkých svätých.
                             Vieme Vám vyrobiť rôzne výrobky aj na objednávku podľa vášho želania, ktoré si môžete objednať prostredníctvom našej mailovej adresy alebo na sociálnych sieťach (Facebook či Instagram).
                             Našu ponuku a hotové výrobky môžte sledovať aj na našich sociálnych sieťach. <br></p>
-                            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;V prípade záujmu nás neváhajte kontaktovať.</text>
+                            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;V prípade záujmu nás neváhajte kontaktovať.<br></p>
+                            <p>
+                            <h4>Platba</h4>
+                        <h6>Dobierka:</h6>Jednoduchý a prehľadný spôsob úhrady za doručený tovar. Celkovú sumu za zásielku uhradíte v hotovosti pri preberaní balíka priamo kuriérovi alebo poštovému doručovateľovi. Tento spôsob je najobľúbenejší a najpoužívanješí. Dobierkový poplatok 0,95€.
+                        <p></p><h6>Prevodný príkaz:</h6>Do poznámky potrebné uviesť meno a priezvisko objednávateľa.<br>IBAN účtu: SK08 0900 000000 0123123123</text>
                     </div>
                     <div class="card-body">
-                        <h5>Kontak na nás</h5>
+                        <h5>Kontakt na nás</h5>
                         <div class="content">
                             <div class="social">
                                 <div class="col-md-3">
