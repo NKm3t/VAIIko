6facb3532bb81b391c55a2cc82c5ad6a23c1d22b
Norbert Kmet <norbertkmet@gmail.com>
2023-01-17 20:21:28 +0100

    Upraveny slider

--- resources/views/layouts/inc/slider.blade.php
+++ resources/views/layouts/inc/slider.blade.php
@@ -6,13 +6,13 @@
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="{{ asset('assets/images/slider/slider_wood.jpg') }}" class="d-block w-100" alt="...">
+            <img src="{{ asset('assets/images/slider/slider_dekoracielussy.jpg') }}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="{{ asset('assets/images/slider/slider_wood.jpg') }}" class="d-block w-100" alt="...">
+            <img src="{{ asset('assets/images/slider/slider_xmas.jpg') }}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="{{ asset('assets/images/slider/slider_wood.jpg') }}" class="d-block w-100" alt="...">
+            <img src="{{ asset('assets/images/slider/slider_dekoracielussy.jpg') }}" class="d-block w-100" alt="...">
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
